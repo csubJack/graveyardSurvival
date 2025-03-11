81168ca561bacc6c25409b85364e60c493668c94
Mig7Gar7 <107518801+Mig7Gar7@users.noreply.github.com>
2025-03-11 09:52:31 -0700

    Update mgarcia6.cpp

--- mgarcia6.cpp
+++ mgarcia6.cpp
@@ -1,1 +1,37 @@
 // empty file for now
+void zombieHealthBar()
+{
+    int zombieHealth = 7;
+    cout << '{';
+    for (int j=0; j < zombieHealth; j++)
+    {
+        if (j < zombieHealth)
+        {
+            cout << '#';
+        }
+        else
+        {
+            cout << ' ';
+        }
+    }
+}
+
+
+
+
+    //zombieHealth-- Under if (dist < (a->radius*a->)
+    //then call for the new healthbar by doing drawHealthBar(zombieHealth));
+    //
+    //
+    //when zombie dies 
+    //if (zombieHealth == 0) {
+    //  cout << "Zombie defeated!" << endl;
+    //  Asteroid *savea = a-> next;
+    //  deleteAsteroid(&g, a);
+    //  a = savea;
+    //  g.nasteroids--;
+    //
+    //Tryingy to find a way to attach the healthbar to the zombie similar to thrust if that works
+    //
+    //      
+                                 
